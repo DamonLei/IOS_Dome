6c8e3032db451159486fb5b89989decf39661c09
lg雷刚 <glei@dst57566.local>
2014-07-31 14:08:42 +0800

    Initial Commit

---
+++ IOS_DOME/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IOS_DOME
+//
+//  Created by lg雷刚 on 14-7-31.
+//  Copyright (c) 2014年 lg雷刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IOS_DOME/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IOS_DOME
+//
+//  Created by lg雷刚 on 14-7-31.
+//  Copyright (c) 2014年 lg雷刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
